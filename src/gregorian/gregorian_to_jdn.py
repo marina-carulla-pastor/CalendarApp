c01849a415c0a233e8d255c33acad2849e5b72e7
Marina Carulla Pastor <david.shnat.5785@gmail.com>
2026-05-31 21:50:44 +0200

    Add function to convert Gregorian date to JDN

---
+++ src/gregorian/gregorian_to_jdn.py
@@ -0,0 +1,16 @@
+import gregorian.py
+
+def to_jdn(year: int, month: int, day: int) -> int:
+    a = (14 - month) // 12
+    y = year + 4800 - a
+    m = month + 12 * a - 3
+
+    return (
+        day
+        + (153 * m + 2) // 5
+        + 365 * y
+        + y // 4
+        - y // 100
+        + y // 400
+        - 32045
+    )
