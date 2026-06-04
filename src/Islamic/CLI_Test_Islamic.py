092cafc4909e881d0f1197334f06d916154b8af3
kyennan <kyennanharry@gmail.com>
2026-06-04 11:20:16 +0100

    Implement CLI for Islamic date conversion
    Add CLI for converting Islamic dates to and from JDN.

---
+++ src/Islamic/CLI_Test_Islamic.py
@@ -0,0 +1,16 @@
+import islamic
+import islamic_to_jdn
+import from_jdn_islamic
+
+to_from = int(input("1. To JDN\n2. From JDN\n"))
+
+if to_from == 1:
+    year = int(input("Year: "))
+    month = int(input("Month: "))
+    day = int(input("Day: "))
+    jdn = islamic_to_jdn.to_jdn(year, month, day)
+    print("JDN:", jdn)
+else:
+    jdn = int(input("JDN: "))
+    date = from_jdn_islamic.from_jdn(jdn)
+    print("islamic:", date)
